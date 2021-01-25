9c265c420df6542d6b32bfe00e632a62526ce6f9
nmaypeterhuang <77961515+nmaypeterhuang@users.noreply.github.com>
2021-01-25 23:25:11 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from initializeData import *
-ini = IniDataset(7)
-ini.setEdgeWeight()
-for i in [1, 2]:
-    for j in [1, 2]:
-        print(i, j)
-        ini = IniWallet(7, i, j)
-        ini.setNodeWallet()
